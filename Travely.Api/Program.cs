14278bd42322e11d8683f734b8e795299e572f39
pullsina <sovya22@gmail.com>
2026-08-20 14:35:42 +0200

    Connect React frontend to ASP.NET API

--- Travely.Api/Program.cs
+++ Travely.Api/Program.cs
@@ -6,7 +6,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+
+{
+
+    options.AddPolicy("ReactApp", policy =>
+
+    {
+
+        policy
+
+            .WithOrigins("http://localhost:5173")
+
+            .AllowAnyHeader()
+
+            .AllowAnyMethod();
+
+    });
+
+});
+
 var app = builder.Build();
+
+app.UseCors("ReactApp");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
