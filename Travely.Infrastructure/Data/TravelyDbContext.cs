a231eb1393560cdd49ec4b6d03188cc2623d9cde
Ditt Namn <linda-maria.m@hotmail.com>
2026-08-25 14:46:57 +0200

    DbContext, DefaultConnection, and related Program configurations

---
+++ Travely.Infrastructure/Data/TravelyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Travely.Infrastructure.Entities;
+using Travely.Shared.Entities;
+
+namespace Travely.Infrastructure.Data
+{
+    // Ärver från Identity genom IdentityDbContext med hjälp av entiteten ApplicationUser 
+    public class TravelyDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TravelyDbContext(DbContextOptions<TravelyDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<UserResult> UserResults { get; set; }
+    }
+}
