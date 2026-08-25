789e529caefd174a2976d4365bae56e0682c4359
Filippa24 <filippa98b@hotmail.se>
2026-08-25 13:31:14 +0200

    country entity + difficulty & continent enums

---
+++ Travely.Shared/Entities/Country.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Travely.Shared.Enums;
+
+namespace Travely.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Capital { get; set; } = string.Empty;
+
+        public Continent Continent { get; set; }
+
+        public Difficulty Difficulty { get; set; }
+
+        public string Fact { get; set; } = string.Empty;
+
+        public string FlagUrl { get; set; } = string.Empty;
+
+        public string FactUrl { get; set; } = string.Empty;
+    }
+}
---
+++ Travely.Shared/Enums/Continent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travely.Shared.Enums
+{
+    public enum Continent
+    {
+        Europe,
+        Asia,
+        Africa,
+        NorthAmerica,
+        SouthAmerica,
+        Oceania
+    }
+}
