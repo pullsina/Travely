5119746dafaeb695a38f8f586bafe2f74a8d7b39
Filippa24 <filippa98b@hotmail.se>
2026-08-25 14:02:26 +0200

    entities completed

---
+++ Travely.Infrastructure/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Travely.Infrastructure.Entities;
+
+namespace Travely.Shared.Entities;
+
+public class ApplicationUser : IdentityUser
+
+{
+    public int Points { get; set; }
+
+    public ICollection<UserResult> Results { get; set; }
+
+}
---
+++ Travely.Infrastructure/Entities/UserResult.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Travely.Shared.Entities;
+
+namespace Travely.Infrastructure.Entities
+{
+    public class UserResult
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }	
+    public ApplicationUser User { get; set; }	
+
+    public int Score { get; set; }
+        public int TotalQuestions { get; set; }
+        public DateTime CompletedAt { get; set; }
+
+    }
+}
