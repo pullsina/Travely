2d312013c0f0795c3f65bfb2430a25e5ecfda153
Ditt Namn <linda-maria.m@hotmail.com>
2026-08-26 16:18:25 +0200

    Reapply "Updated project referenses"
    This reverts commit 4a84b8863421a7e9bfef422b8da85cf00dfc1f0f.

---
+++ Travely.Api/Controllers/QuizController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Travely.Application.Interfaces;
+
+namespace Travely.Api.Controllers
+{
+    [ApiController]
+    [Route("api/quiz")]
+    public class QuizController : ControllerBase
+    {
+        private readonly IQuizService _quizService;
+
+        public QuizController(IQuizService quizService)
+        {
+            _quizService = quizService;
+        }
+
+    }
+}
+//[ApiController]
+//[Route("api/[controller]")]
+//public class QuizController : ControllerBase
+//{
+//    private readonly IQuizService _quizService;
+
+//    public QuizController(IQuizService quizService)
+//    {
+//        _quizService = quizService;
+//    }
+
+//    [HttpGet("question/{questionId:int}")]
+//    public async Task<ActionResult<QuizQuestionDto>> GetQuestion(
+//        int questionId)
+//    {
+//        var question =
+//            await _quizService.GetQuestionAsync(questionId);
+
+//        if (question == null)
+//            return NotFound();
+
+//        return Ok(question);
+//    }
+
+//    [HttpPost("answer")]
+//    public async Task<ActionResult<SubmitAnswerResultDto>> SubmitAnswer(
+//        SubmitAnswerDto dto)
+//    {
+//        var result =
+//            await _quizService.SubmitAnswerAsync(dto);
+
+//        if (result == null)
+//            return NotFound();
+
+//        return Ok(result);
+//    }
+
+//}
