d1e242cc878ac8e8f6b7f07b82713da85623376b
Ditt Namn <linda-maria.m@hotmail.com>
2026-08-26 15:30:31 +0200

    DTOs

---
+++ Travely.Shared/DTOs/SubmitAnswerDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travely.Shared.DTOs
+{
+    public class SubmitAnswerDto
+    {
+        // Skickar in fråge-id och svarsalternativets id för att kunna spara användarens svar i databasen
+        public int QuestionId { get; set; }
+        public int AnswerId { get; set; } // Selected Country
+    }
+}
---
+++ Travely.Shared/DTOs/SubmitAnswerResultDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Travely.Shared.DTOs
+{
+    public class SubmitAnswerResultDto
+    {
+        public bool IsCorrect { get; set; }
+
+        public int QuestionId { get; set; }
+
+        public int CorrectAnswerId { get; set; }
+
+        public int? Score { get; set; }
+    }
+}
