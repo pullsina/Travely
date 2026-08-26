c6ecd8a3ef3ecc45e397e6e0a7cb1959c564c69e
Ditt Namn <linda-maria.m@hotmail.com>
2026-08-26 15:48:12 +0200

    Created folder and files

---
+++ Travely.Infrastructure/Repositories/QuizRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Travely.Infrastructure.Interfaces;
+using Travely.Shared.Entities;
+
+namespace Travely.Infrastructure.Repositories
+{
+    public class QuizRepository : IQuizRepository
+    {
+        public Task<Country?> GetQuestionAsync(int questionId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
