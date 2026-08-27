27a6ab06ba7c89458fd2c9b5b78fac808e5be0f9
Ditt Namn <linda-maria.m@hotmail.com>
2026-08-27 14:29:52 +0200

    IQuizRepo moved, IQuizRepo and QuizRepo updated, project references checked

---
+++ Travely.Application/Interfaces/IQuizRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Travely.Shared.DTOs;
+
+namespace Travely.Application.Interfaces
+{
+    public interface IQuizRepository
+    {
+        // Interfaces use DTOs 
+        Task<QuizQuestionDto?> GetQuestionAsync(int questionId);
+
+        Task<List<QuizAnswerDto>> GetAnswerOptionsAsync(
+            int correctAnswerId,
+            int numberOfOptions);
+    }
+}
--- Travely.Infrastructure/Repositories/QuizRepository.cs
+++ Travely.Infrastructure/Repositories/QuizRepository.cs
@@ -1,16 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Travely.Infrastructure.Interfaces;
-using Travely.Shared.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using Travely.Application.Interfaces;
+using Travely.Infrastructure.Data;
+using Travely.Infrastructure.Entities;
+using Travely.Shared.DTOs;
 
 namespace Travely.Infrastructure.Repositories
 {
     public class QuizRepository : IQuizRepository
     {
-        public Task<Country?> GetQuestionAsync(int questionId)
+        private readonly TravelyDbContext _context;
+
+        public QuizRepository(TravelyDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<QuizQuestionDto?> GetQuestionAsync(
+            int questionId)
+        {
+            var country = await _context.Countries
+                .FirstOrDefaultAsync(c => c.Id == questionId);
+
+            if (country == null)
+            {
+                return null;
+            }
+
+            return new QuizQuestionDto
+            {
+                QuestionId = country.Id,
+                Capital = country.Capital
+            };
+        }
+
+        public async Task<List<QuizAnswerDto>> GetAnswerOptionsAsync(
+            int correctAnswerId,
+            int numberOfOptions)
+        {
+            var countries = await _context.Countries
+                .Where(c => c.Id != correctAnswerId)
+                .OrderBy(c => Guid.NewGuid())
+                .Take(numberOfOptions - 1)
+                .ToListAsync();
+
+            return countries
+                .Select(c => new QuizAnswerDto
+                {
+                    Id = c.Id,
+                    Answer = c.Name
+                })
+                .ToList();
         }
     }
 }
