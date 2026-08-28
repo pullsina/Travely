d6064d3bf480f8e31d8af5159799a591d14674b5
Ditt Namn <linda-maria.m@hotmail.com>
2026-08-28 14:36:39 +0200

    Added: - using ... Appl.Services - Comments (Headline comments) - Services - Repositories - Deleted old LMM comments and outcommented code

--- Travely.Api/Program.cs
+++ Travely.Api/Program.cs
@@ -2,41 +2,54 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Travely.Application.Interfaces;
+using Travely.Application.Services;
 using Travely.Infrastructure.Data;
+using Travely.Infrastructure.Repositories;
 using Travely.Infrastructure.Services;
 using Travely.Shared.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+//------------------------------------
+// Controllers
+//------------------------------------
 builder.Services.AddControllers();
+
+//------------------------------------
+// Database 
+//------------------------------------
 // Add connection to sql server 
 builder.Services.AddDbContext<TravelyDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-//// configurations for db context - to be implemented
-//builder.Services
-//    .AddIdentityCore<ApplicationUser>()
-//    .AddRoles<IdentityRole>()
-//    .AddEntityFrameworkStores<TravelyDbContext>();
-//// authentication/authorization - to be implemented 
-//builder.Services.AddAuthentication();
-//builder.Services.AddAuthorization();
 
-//Need this for identity-------------:
+//------------------------------------
+// ASP.NET Core Identity
+//------------------------------------
 builder.Services
     .AddIdentityCore<ApplicationUser>()
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<TravelyDbContext>()
     .AddSignInManager();
-                                 //namn på cookie som ska användas
+
+//------------------------------------
+// Authentication and Authorization
+//------------------------------------
+//namn på cookie som ska användas
 builder.Services.AddAuthentication(IdentityConstants.ApplicationScheme)
     .AddIdentityCookies();
-
 builder.Services.AddAuthorization();
-//------------------------------------
 
+//------------------------------------
+// Application services
+//------------------------------------
 builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IQuizService, QuizService>();
+
+//------------------------------------
+// Infrastructrure repositories
+//------------------------------------
+builder.Services.AddScoped<IQuizRepository, QuizRepository>();
+//------------------------------------
 
 //not needed since we use our own auth controller and service for register/login/logout
 //builder.Services.AddIdentityApiEndpoints<ApplicationUser>().AddEntityFrameworkStores<TravelyDbContext>();
@@ -45,34 +58,32 @@ builder.Services.AddScoped<IAuthService, AuthService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 //builder.Services.AddOpenApi();
 
+//------------------------------------
 //for testing endpoints in swagger
+//------------------------------------
 builder.Services.AddSwaggerGen();
 
+//------------------------------------
+// CORS React/Vte
+//------------------------------------
 builder.Services.AddCors(options =>
-
 {
-
     options.AddPolicy("ReactApp", policy =>
-
     {
-
         policy
-
             .WithOrigins("http://localhost:5173")
-
             .AllowAnyHeader()
-
             .AllowAnyMethod();
-
     });
-
 });
 
 var app = builder.Build();
 
 app.UseCors("ReactApp");
 
-// Configure the HTTP request pipeline.
+//------------------------------------
+// Configure the HTTP request pipeline
+//------------------------------------
 if (app.Environment.IsDevelopment())
 {
     //app.MapOpenApi();
