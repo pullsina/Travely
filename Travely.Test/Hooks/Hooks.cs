be5a25f02555186f0576a5fbf7785581c7d231a4
Filippa24 <filippa98b@hotmail.se>
2026-09-01 14:17:40 +0200

    Created test project. Test for succeeded login complete. Hooks for set up and clean up before and after tests

---
+++ Travely.Test/Hooks/Hooks.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Playwright;
+using Reqnroll;
+
+namespace Travely.Tests.Hooks;
+
+//hooks to set up and clean up the browser and page for the tests
+
+//binding to connect hooks to the feature files
+[Binding]
+public class Hooks
+{
+    private IPlaywright _playwright = null!;
+    private IBrowser _browser = null!;
+
+    //the page playwright uses to do the tests
+    public IPage Page { get; private set; } = null!;
+
+    //SETUP - runs before each scenario to set up and prepare the browser and page for the tests
+    [BeforeScenario]
+    public async Task BeforeScenario()
+    {
+        _playwright = await Playwright.CreateAsync();
+
+        _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            //headless false for us to see the frontend during tests
+            Headless = false
+        });
+
+        Page = await _browser.NewPageAsync();
+    }
+
+    //CLEANUP - runs after the tests to clean up and close the browser and page
+    [AfterScenario]
+    public async Task AfterScenario()
+    {
+        await _browser.CloseAsync();
+        _playwright.Dispose();
+    }
+}
---
+++ Travely.Test/StepDefinitions/LoginSteps.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Playwright;
+using Reqnroll;
+using Travely.Tests.Hooks;
+
+namespace Travely.Tests.StepDefinitions;
+
+//binding to connect the steps to the feature files
+[Binding]
+public class LoginSteps
+{
+    private readonly Hooks.Hooks _hooks;
+
+    //konstructor
+    public LoginSteps(Hooks.Hooks hooks)
+    {
+        _hooks = hooks;
+    }
+
+    //match the steps in the feauture file, word by word to the methods
+    [Given("I am on the login page")]
+    public async Task GivenIAmOnTheLoginPage()
+    {
+        //open page via hooks
+        await _hooks.Page.GotoAsync("http://localhost:5173/login");
+    }
+
+    //match the steps in the feauture file, {string} means Reqnroll uses the values from the feature file as parameters (no hardcoded values)
+    [When("I fill in email {string} and password {string}")]
+    public async Task WhenIFillInEmailAndPassword(string email, string password)
+    {
+        //playwright searches for label "Email" in frontend and fills with the value from the feature file
+        await _hooks.Page.GetByLabel("Email").FillAsync(email);
+        await _hooks.Page.GetByLabel("Password").FillAsync(password);
+    }
+
+    [When("I click the login button")]
+    public async Task WhenIClickTheLoginButton()
+    {
+        //find the button named "Login" and click it
+        await _hooks.Page.GetByRole(AriaRole.Button, new()
+        {
+            Name = "Log in"
+        }).ClickAsync();
+    }
+
+    [Then("I should be redirected to the continents page")]
+    public async Task ThenIShouldBeRedirectedToTheContinentsPage()
+    {
+        //** = the url can contain whatever before /continents, avoids hardcoding the url in case it changes
+        await _hooks.Page.WaitForURLAsync("**/continents");
+
+        Assert.That(_hooks.Page.Url, Does.Contain("/continents"));
+    }
+}
