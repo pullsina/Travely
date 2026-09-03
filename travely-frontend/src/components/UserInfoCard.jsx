5c64541bdf82a9d1f762b9de860621dc6dc406d9
Ditt Namn <linda-maria.m@hotmail.com>
2026-09-03 14:34:13 +0200

    Further adjustments in profilepage and cards

--- travely-frontend/src/components/UserInfoCard.jsx
+++ travely-frontend/src/components/UserInfoCard.jsx
@@ -7,51 +7,46 @@ function UserInfoCard({ user, onClose }) {
 
   return (
     <section className="user-info-card" aria-label="User details card">
+      <div className="user-info-card__info" aria-label="User details">
+        <p
+          className="user-info-card__text"
+          aria-labelledby="user-info-card__text"
+        >
+          <strong>Name:</strong> {user.username}
+        </p>
+        <p
+          className="user-info-card__text"
+          aria-labelledby="user-info-card__text"
+        >
+          <strong>Email:</strong> {email || "No email available"}
+        </p>
+      </div>
+      <div className="user-info-card__panel-actions" aria-label="User actions">
+        {/* Buttons for actions */}
+        <button
+          className="primary-button user-info-card__change-info-button"
+          type="button"
+          aria-label="Change user info - Not implemented"
+        >
+          Change Info
+        </button>
+        <p className="user-info-card__text-small">To be implemented!</p>
+        <button
+          className="primary-button user-info-card__delete-profile-button"
+          type="button"
+          aria-label="Delete user profile - Not implemented"
+        >
+          Delete Profile
+        </button>
+        <p className="user-info-card__text-small">To be implemented!</p>
+      </div>
       <button
-        className="primary-button user-info-card__back-button"
+        className="primary-button user-info-card__actions__close-button"
         type="button"
         onClick={onClose}
       >
-        Back to profile
+        Close
       </button>
-      <div className="user-info-card__body-panel">
-        <div className="user-info-card__panel-info" aria-label="User details">
-          <p
-            className="user-info-card__text"
-            aria-labelledby="user-info-card__text"
-          >
-            <strong>Name:</strong> {user.username}
-          </p>
-          <p
-            className="user-info-card__text"
-            aria-labelledby="user-info-card__text"
-          >
-            <strong>Email:</strong> {email || "No email available"}
-          </p>
-        </div>
-        <div
-          className="user-info-card__panel-actions"
-          aria-label="User actions"
-        >
-          {/* Buttons for actions */}
-          <button
-            className="primary-button user-info-card__change-info-button"
-            type="button"
-            aria-label="Change user info - Not implemented"
-          >
-            Change Info
-          </button>
-          <p className="user-info-card__text-small">To be implemented!</p>
-          <button
-            className="primary-button user-info-card__delete-profile-button"
-            type="button"
-            aria-label="Delete user profile - Not implemented"
-          >
-            Delete Profile
-          </button>
-          <p className="user-info-card__text-small">To be implemented!</p>
-        </div>
-      </div>
     </section>
   );
 }
