289abac455ee32468d9a43f2ff9bf91c43725ceb
Filippa24 <filippa98b@hotmail.se>
2026-09-08 14:35:26 +0200

    changes for deployment

--- Travely.Api/Program.cs
+++ Travely.Api/Program.cs
@@ -112,11 +112,8 @@ app.UseCors("ReactApp");
 //------------------------------------
 // Configure the HTTP request pipeline
 //------------------------------------
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
