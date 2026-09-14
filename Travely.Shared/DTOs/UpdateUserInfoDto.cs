f14a51352e53296e216a3809b28ee229013b7530
Ditt Namn <linda-maria.m@hotmail.com>
2026-09-14 09:14:51 +0200

    Created DTO

---
+++ Travely.Shared/DTOs/UpdateUserInfoDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Travely.Shared.DTOs
+{
+    public class UpdateUserInfoDto
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
